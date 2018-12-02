42e9d20246bddcd4661cff1ee356aaa91a946466
cuxunited <45526880+cuxunited@users.noreply.github.com>
2018-12-02 10:30:05 -0600

    added loop and counter

--- dotsandbox.py
+++ dotsandbox.py
@@ -1,22 +1,34 @@
 import numpy as np
 
-dot = "Q"
+count=33
+dots = 5
 length = 7
 width = 7
 grid = np.zeros((length, width))
 for i in range(length):
     for j in range(width):
         if i % 2 == 0 and j % 2 == 0:
-            grid[i][j] = 5
+            grid[i][j] = dots
 
-rownum = input()
-colnum = input()
-grid[int(rownum)][int(colnum)]=1
+while count != 0:
+ 
+    rownum = input()
+    colnum = input()
 
+    if grid[0][1] == 0:
+        grid[int(rownum)][int(colnum)]=input()
+        print(grid)
+        print("moves left:")
+        print(count)
+        count = count-1
 
+    elif grid[0][1] and grid[1][0]:
+        grid[int(rownum)][int(colnum)]=7
 
-
-
+    elif count == 0:
+        break
 
 print(grid)
-
+print("moves left:")
+print(count)
+        
